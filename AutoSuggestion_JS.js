28518bb2d7c581d18e46bc44ebb5bcf99832cb6e
ankitasharma22 <41355156+ankitasharma22@users.noreply.github.com>
2018-08-22 21:13:25 +0530

    Implemented version1 of auto-suggestion box

---
+++ AutoSuggestion_JS.js
@@ -0,0 +1,43 @@
+function Search() {
+    debugger;
+    document.getElementById("suggestion").innerHTML = "";
+
+    var names = ["Ragul", "Rajpreet", "Pallvi", "Neha", "Ankita", "Raja", "Shreea", "Smriti", "Shrijeet", "Ayush", "Swapnil", "Nihit", "Bhargavi", "Anushka", "Swinal", "Utkarsh", "Saurabh", "Paarth", "Vishwas", "Mohit", "Gurbaksh", "Ashwarya"];      
+    names.sort();
+    var countOfCharMatching = 0;
+    var isCharPresent = "", listData = "";
+    var currentArrayName = ""; 
+        var inputBox = document.getElementById("input").value;
+        var initialSuggestions = "";
+        if(inputBox.length>0)
+        {
+        for (i = 0; i < names.length; i++) { 
+
+            currentArrayName = names[i].toUpperCase();
+            currentInputBoxValue = inputBox.toUpperCase();
+
+            if(inputBox.length > 0)
+                isCharPresent = currentArrayName.includes(currentInputBoxValue);
+
+            if(isCharPresent === true) {
+                listData = "<li id='"+i+"'  onclick='clickOnList(this.innerHTML)'>"+ names[i] + "</li>";
+                document.getElementById("suggestion").innerHTML =initialSuggestions + listData;
+                countOfCharMatching = countOfCharMatching + 1;
+                initialSuggestions = document.getElementById("suggestion").innerHTML;
+            }
+            else {
+                document.getElementById("suggestion").innerHTML = "No suggestions";
+            }
+        }
+        }
+    }
+
+
+function clickOnList(valueOfList) {
+    document.getElementById("input").value = valueOfList;
+    document.getElementById("suggestion").innerHTML = "";
+}
+
+function goOnPage() {
+    document.getElementById("suggestion").innerHTML = "";
+}
